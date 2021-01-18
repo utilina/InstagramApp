6007495eca3aa0af1f846ff74abec105e8ef9dbc
Anastasiya <asulitina@stream.ru>
2021-01-18 16:10:06 +0300

    Initial Commit

---
+++ InstagramApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramApp
+//
+//  Created by Улитина Анастасия Сергеевна on 18.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
