52bd0965915c40a4546edc5273678a8c3f9798f0
Улитина Анастасия Сергеевна <asulitina@stream.ru>
2021-01-28 18:39:43 +0300

    profile settings

--- InstagramApp/Feed/View/FeedCell.swift
+++ InstagramApp/Feed/View/FeedCell.swift
@@ -11,10 +11,11 @@ class FeedCell: UICollectionViewCell {
 
 	// MARK: - Constants
 
-	enum Constants {
-//		case profileImageViewHeight(Int) = 40
-//		case profileImageViewWidth(Int) = 40
-	}
+    private struct Constants {
+        static let profileImageViewHeight = CGFloat(40)
+        static let profileImageViewWidth = CGFloat(40)
+        static let paddings = CGFloat(12)
+    }
 
 	// MARK: - Views
 
@@ -31,12 +32,42 @@ class FeedCell: UICollectionViewCell {
 		return iv
 	}()
 
+    private lazy var userNameButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitleColor(.black, for: .normal)
+        button.setTitle("venom", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 13)
+        button.addTarget(self, action: #selector(didTapUserName), for: .touchUpInside)
+        return button
+    }()
+
+    private let postImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.isUserInteractionEnabled = true
+        iv.image = #imageLiteral(resourceName: "venom-7")
+        return iv
+    }()
+
+    private lazy var likeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitleColor(.black, for: .normal)
+        button.setTitle("venom", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 13)
+        button.addTarget(self, action: #selector(didTapUserName), for: .touchUpInside)
+        return button
+    }()
+
 	// MARK: - Initialization
 
 	override init(frame: CGRect) {
 		super.init(frame: frame)
 
 		backgroundColor = .white
+        profileImageViewConfig()
+        userNameButtonConfig()
+
 	}
 	
 	required init?(coder: NSCoder) {
@@ -47,14 +78,31 @@ class FeedCell: UICollectionViewCell {
 
 	// MARK: - Functions
 
-	func profileImageViewConfig() {
-		addSubview(profileImageView)
-		profileImageView.anchor(top: topAnchor, left: leftAnchor, paddingTop: 12, paddingLeft: 12)
-		profileImageView.setDimensions(height: 40, width: 40)
-		profileImageView.layer.cornerRadius = 40 / 2
-	}
-
 	// MARK: - Private functions
+
+    private func profileImageViewConfig() {
+        addSubview(profileImageView)
+        profileImageView.anchor(top: topAnchor,
+                                left: leftAnchor,
+                                paddingTop: Constants.paddings,
+                                paddingLeft: Constants.paddings)
+        profileImageView.setDimensions(height: Constants.profileImageViewHeight,
+                                       width: Constants.profileImageViewWidth)
+        profileImageView.layer.cornerRadius = Constants.profileImageViewHeight / 2
+    }
+
+    private func  userNameButtonConfig() {
+        addSubview(userNameButton)
+        userNameButton.centerY(inView: profileImageView,
+                               leftAnchor: profileImageView.rightAnchor,
+                               paddingLeft: 8)
+    }
+
+    // MARK: - Actions
+
+    @objc func didTapUserName() {
+        print("so,me")
+    }
 
 
 }
